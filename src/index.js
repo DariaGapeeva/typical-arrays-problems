237d7c037f897bf0a330ff619527c1d6c4c490e8
DariaGapeeva <d.gapeeva@gmail.com>
2021-01-30 17:41:10 +0700

    feat: do tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    let minOfArray = array.reduce(
+        (min, item) => (item < min ? item : min),
+        array[0]
+    );
 
-exports.max = function max (array) {
-  return 0;
-}
+    return minOfArray;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    let maxOfArray = array.reduce(
+        (max, item) => (item > max ? item : max),
+        array[0]
+    );
+
+    return maxOfArray;
+};
+
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    let sum = array.reduce((sum, item) => sum + item, 0);
+    return sum / array.length;
+};
